7c41d7aef70e68bbb2de3ea55ca52334fe5a5d61
Suh Seung Ju <78189429+seunzu@users.noreply.github.com>
2024-09-02 00:27:37 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -54,7 +54,6 @@ app.use(passport.session());
 
 app.use("/auth", authRouter);
 app.use("/", marketRouter);
-app.use("/account", accountRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({
