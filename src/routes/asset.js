48c308062a97f61e6b62a267f9b13725fa910971
zzoo0123 <0123suh@gmail.com>
2024-09-04 18:16:17 +0900

    test:

--- src/routes/asset.js
+++ src/routes/asset.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { isLoggedIn } = require("../middlewares");
-const { getSavedAssetData } = require("../api/controllers/asset");
+const { getAssetData } = require("../api/controllers/asset");
+// const { getSavedAssetData } = require("../api/controllers/asset");
 const router = express.Router();
 
 // GET /assets
 // router.get("/assets", isLoggedIn, getSavedAssetData);
-router.get("/assets", getSavedAssetData);
+router.get("/assets", getAssetData);
 
 module.exports = router;
