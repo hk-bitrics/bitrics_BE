b939b446d089d2422badcb7bf39d7cb4dbcf9854
zzoo0123 <0123suh@gmail.com>
2024-09-04 23:45:36 +0900

    fix: cors error

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ app.use(
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-      httpOnly: false,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
     },
   })
