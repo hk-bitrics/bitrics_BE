d237372e2e27c0a0725491b6c3013502e73ee633
zzoo0123 <0123suh@gmail.com>
2024-09-04 15:17:20 +0900

    refactor: 로직 분리

--- src/api/controllers/asset.js
+++ src/api/controllers/asset.js
@@ -1,12 +1,13 @@
-const { getAssetData, getSavedAssetData } = require("../services/asset");
+const { removeSaveAssetData, getSavedAssetData } = require("../services/asset");
 
 exports.getSavedAssetData = async (req, res) => {
   try {
     const userId = req.user.user_id;
-    await getAssetData(userId);
+    await removeSaveAssetData(userId);
     const data = await getSavedAssetData(userId);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
+    console.error(error);
   }
 };
