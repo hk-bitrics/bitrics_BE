6c1adff1345a4ff866e67fc9cf17bbe4548bbca6
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-04 13:15:38 +0900

    fix: 뉴스 크롤링시 keyword 선택을 위한 path variable 추가

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getNews } = require("../api/controllers/news");
 const router = express.Router();
 
-router.get("/news", getNews);
+router.get("/news/:keyword", getNews);
 
 module.exports = router;
