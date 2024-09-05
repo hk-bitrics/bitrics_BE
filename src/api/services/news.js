7fbb12bb0f476d451cf31da154fe656d56c36e60
Chaemin-L <chaem12@kookmin.ac.kr>
2024-09-05 16:28:59 +0900

    fix: news 스크래핑 에러 해결

--- src/api/services/news.js
+++ src/api/services/news.js
@@ -23,12 +23,12 @@ const getNews = async (keyword) => {
   try {
     const newsList = $(".article .txt_wrap")
       .map(function (i, el) {
-        const title = $(el)._findBySelector("a > em.tit").text();
-        const content = $(el)._findBySelector("a > p.txt").text();
-        const href = $(el)._findBySelector("a").attr("href");
-        const media = $(el)._findBySelector("p.info span:first").text();
-        const createdAt = $(el)._findBySelector("p.info span:last").text();
-        let thumbnail = $(el)._findBySelector(".thumbnail img").attr("src");
+        const title = $(el).find("a > em.tit").text();
+        const content = $(el).find("p.txt").text();
+        const href = $(el).find("a").attr("href");
+        const media = $(el).find("p.info span:first").text();
+        const createdAt = $(el).find("p.info span:last").text();
+        let thumbnail = $(el).find(".thumbnail img").attr("src");
         thumbnail = thumbnail.startsWith("//") ? null : thumbnail;
         return { title, content, href, media, createdAt, thumbnail };
       })
